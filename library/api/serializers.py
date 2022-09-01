b87e033827f7d4e698999531c1ee7861915bb819
Faustina F. Honsiah <fhonsiah@gmail.com>
2022-09-01 09:29:08 +0300

    initial commit

---
+++ library/api/serializers.py
@@ -0,0 +1,11 @@
+# api/serializers.py
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from books.models import Book
+
+
+class BookSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Book
+        fields = ('title', 'subtitle', 'author', 'isbn')
